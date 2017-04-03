d04dca268079442b127371216de38b59b9322ff3
yetzt <git@yetzt.me>
2017-04-03 12:54:57 +0200

    add questions

--- assets/data/data.js
+++ assets/data/data.js
@@ -12,48 +12,75 @@ window.omatdata = {
 	"parties": [
 		"linke",
 		"piraten",
-		"afd"
+		"afd",
+		"fdp",
+		"cdu",
+		"spd"
 	],
 	"answers": [
 		[
 			[2,"Wir setzen uns für die Nutzung und die Erstellung offener Lehr- und Lernmaterialien (Open Educational Resources, OER) sowie den Einsatz von Open-Source-Software ein."],
 			[2,"Im Bildungskontext soll die Mediennutzung frei von Urheberrechtsabgaben erfolgen können. Bildung ist ein viel zu wichtiges Gut, um es unnötig vielen Einschränkungen zu unterwerfen. Bildung ist Motor für Wissen, Wirtschaft, Innovation und Kreativität, und unentbehrlich für die Weiterentwicklung einer Gesellschaft sowie der souveränen Teilhabe ihrer Mitglieder an dieser.\n\nDes Weiteren sollen in diesem Bereich zusätzlich neue Geschäftsmodelle auf der Basis von freien Lizenzen angeregt werden."],
-			[2,"Die AfD fordert im Wahlprogramm für NRW in These 1.11: \"Die Lehrkraft hat als fachliche Autorität wieder für einen hohen Lernertrag zu sorgen.\" In diesem Sinne erscheint es durchaus sinnvoll, die innerhalb der bezahlten Arbeitstätigkeit erstellten Materialien auch anderen Kollegen zur Verfügung zu stellen. Grundsätzlich sehen wir aber in einer so weitgehenden Interpretation unserer Forderung Durchsetzungsschwierigkeiten."]
+			[2,"Die AfD fordert im Wahlprogramm für NRW in These 1.11: \"Die Lehrkraft hat als fachliche Autorität wieder für einen hohen Lernertrag zu sorgen.\" In diesem Sinne erscheint es durchaus sinnvoll, die innerhalb der bezahlten Arbeitstätigkeit erstellten Materialien auch anderen Kollegen zur Verfügung zu stellen. Grundsätzlich sehen wir aber in einer so weitgehenden Interpretation unserer Forderung Durchsetzungsschwierigkeiten."],
+			[0,"Die Freien Demokraten wollen die Lehrkräfte z.B. mit einer Fortbildungsoffensive dabei unterstützen, die Chancen der Digitalisierung in der Schule und damit auch die vielfältigen Chancen digitaler Bildungsmedien etwa für das Bearbeiten, Teilen und Kooperieren viel stärker zu nutzen. Allerdings wollen wir Lehrkräfte bei von ihnen erarbeiteten Inhalte nicht dazu verpflichten, diese automatisch zu veröffentlichen. Dies wäre nicht nur urheberrechtlich problematisch. Neben dem bürokratischen Aufwand könnte dies auch zu einer geringeren Bereitschaft führen, solche Materialien überhaupt zu erstellen."],
+			[0,"Wir sind für die pädagogische Freiheit von Lehrerinnen und Lehrern. Deshalb sollen sie selbst darüber entscheiden können. Lehrkräfte, die ihre Materialien weitergeben wollen, sollte das Land aber dabei unterstützen."],
+			[1,"Die SPD hat diese Frage bislang nicht beantwortet."]
 		],
 		[
 			[0,"Wer die Eingriffsbefugnisse des Staates noch mehr ausweiten will, entkernt die Grundrechte, bis von ihnen nichts mehr übrig ist."],
 			[0,"In einem verdeckten Eingriff in informationstechnische Systeme (z.B. mittels so genannter Bundes- oder Staatstrojaner) durch staatliche Stellen erkennen wir immer einen unverhältnismässigen Eingriff in das Grundrecht auf Gewährleistung der Vertraulichkeit und Integrität informationstechnischer Systeme. Gesetze, die dem Staat derart tiefe Eingriffe in den Kernbereich privater Lebensgestaltung seiner Bürger erlauben, sind nicht mit unserer Auffassung von Grundrechten und Rechtstaat vereinbar.\n\nWir streben ein vollständiges Verbot von Staatstrojanern und aller unter diesem Begriff und zu den gleichen Zielen entwickelter Werkzeugen an."],
-			[2,"Die AfD tritt für die Grundrechte ein, die nur in engem Rahmen eingeschränkt werden dürfen."]
+			[2,"Die AfD tritt für die Grundrechte ein, die nur in engem Rahmen eingeschränkt werden dürfen."],
+			[2,"So wie offline bzw. körperlich erfolgende Eingriffe der Behörden durch Sicherstellung eines Smartphones oder Computers – ggfs. im Rahmen einer Durchsuchung – und dessen technische Auswertung dürfen auch bei Nutzung der „Onlinewege“ staatliche Maßnahmen nur anlass- und verdachtsabhängig und nur im Rahmen klarer gesetzlicher rechtsstaatlicher Regelungen sowie erforderlicher richterlicher Beschlüsse erfolgen."],
+			[2,"Staatliches Hacking darf nur in Form eines Durchsuchungsbeschlusses erlaubt sein."],
+			[1,"Die SPD hat diese Frage bislang nicht beantwortet."]
 		],
 		[
 			[0,"Einsatz von „predictive policing“ gesetzlich verbieten”\n\n“Der Schutz personenbezogener Daten muss gestärkt werden. Dafür wollen wir das Datenschutzgesetz NRW weiterentwickeln und unnötige Datensammlungen abschaffen.\n\nWas tun?\n\nRecht auf Anonymität im Internet und Kontrolle über die eigenen Daten\n\nLandesbeauftragte für Datenschutz und Informationsfreiheit stärken\n\nÜber eine Bundesratsinitiative die Vorratsdatenspeicherung abschaffen\n\nVerbot des Exports von Hard- und Software für Überwachung und/oder Zensur”\n\nStatt Sonderregelungen und Erlasse gegen Migrant*innen fordern wir die volle rechtliche Gleichstellung durch erleichterte Einbürgerung und das Wahlrecht für alle, die hier dauerhaft leben.\n\nErgänzt werden muss eine solche Politik durch einen konsequenten Ausbau des Diskriminierungsschutzes auf allen Ebenen. Das Land NRW muss auf Landesebene alle diskriminierenden und ausgrenzenden Gesetze und Vorschriften abschaffen und sich auf Bundesebene für die Beseitigung solcher einsetzen. Das ist auch ein zentraler Baustein im Kampf gegen Rassismus und Diskriminierung."],
 			[0,"Predictive Policing bezeichnet eine Möglichkeit Verbrechen anhand von Statistiken und anderen Informationen vor deren Eintreten vorherzusagen. Diese Methoden weisen ein nicht zu vernachlässigendes Fehlerrisiko auf und eine Aussage zu den Erfolgen ist derzeit noch schwer.\n\nGrundlage für die Statistiken und anderen Informationsquellen sind Daten, die zuvor gesammelt wurden. Das steht nach unserer Auffassung im Widerspruch zu unserer Forderung zur Datensparsamkeit. Wir sehen diese Art der Polizeiarbeit daher kritisch."],
-			[2,"Aufgrund der hohen Anzahl von Personen, die ohne Ausweisdokumente einreisen und um Asyl ersuchen, ist es sinnvoll, auf diese Daten zuzugreifen. Dies ergibt sich indirekt aus These 6.04 des Wahlprogramms der AfD für NRW: \"Die AfD wird Asylmissbrauch verhindern. Dem Missbrauch des Asylrechts zu Einwanderungszwecken und zur Erlangung sozialer Leistungen ist ein Riegel vorzuschieben.\""]
+			[2,"Aufgrund der hohen Anzahl von Personen, die ohne Ausweisdokumente einreisen und um Asyl ersuchen, ist es sinnvoll, auf diese Daten zuzugreifen. Dies ergibt sich indirekt aus These 6.04 des Wahlprogramms der AfD für NRW: \"Die AfD wird Asylmissbrauch verhindern. Dem Missbrauch des Asylrechts zu Einwanderungszwecken und zur Erlangung sozialer Leistungen ist ein Riegel vorzuschieben.\""],
+			[0,"Anlasslose (!) Auswertungen lehnen wir ab: So wie offline bzw. körperlich erfolgende Eingriffe der Behörden dürfen auch bei Nutzung der „Onlinewege“ staatliche Maßnahmen nur anlass- und verdachtsabhängig und nur im Rahmen klarer gesetzlicher rechtsstaatlicher Regelungen sowie ggfs. erforderlicher richterlicher Beschlüsse erfolgen."],
+			[0,"Wir begrüßen, dass eine Rechtsgrundlage im Asylgesetz geschaffen wird, wonach das Bundesamt für Migration und Flüchtlinge – ebenso wie bereits die Ausländerbehörden – nur in begründeten Einzelfällen zur Sicherung, Feststellung und Überprüfung der Identität einschließlich der Staatsangehörigkeit von Asylsuchenden Daten aus Datenträgern herausverlangen und auswerten kann."],
+			[1,"Die SPD hat diese Frage bislang nicht beantwortet."]
 		],
 		[
 			[2,"Wir setzen uns für die Nutzung und die Erstellung offener Lehr- und Lernmaterialien (Open Educational Resources, OER) sowie den Einsatz von Open-Source-Software ein."],
 			[2,"Wir setzen uns dafür ein, dass jegliche durch das Land NRW genutzte, freie Open Source Software finanziell unterstützt wird.\n\nDes Weiteren setzen wir uns dafür ein, dass das Land NRW einen Fördertopf zur allgemeinen Unterstützung freier Open Source Software aufbaut."],
-			[0,"Die AfD tritt in ihrem Wahlprogramm für NRW ein gem. These 4.07 für eine Gesundung des Landeshaushaltes und für einen Straftatbestand \"Steuerverschwendung\". Insofern sind auch bei Softwareprojekten vor allem Kostenaspekte zu berücksichtigen. Dies umfasst nachvollziehbare Abschätzungen der Folgekosten."]
+			[0,"Die AfD tritt in ihrem Wahlprogramm für NRW ein gem. These 4.07 für eine Gesundung des Landeshaushaltes und für einen Straftatbestand \"Steuerverschwendung\". Insofern sind auch bei Softwareprojekten vor allem Kostenaspekte zu berücksichtigen. Dies umfasst nachvollziehbare Abschätzungen der Folgekosten."],
+			[0,"Bei Beschaffung und Einsatz von IT sollen Nutzer-, Bürger- bzw. Kundenfreundlichkeit und Effizienz im Mittelpunkt stehen."],
+			[0,"Ebenso wie das Bundesamt für die Sicherheit in der Informationstechnik vertritt die CDU die Auffassung, dass freie und quelloffene Software inzwischen eine anerkannte Alternative zu proprietären Angeboten darstellt. Gleichwohl unterliegen die Einsatzmöglichkeiten dieser Software gewissen Rahmenbedingungen, die beachtet werden müssen. Hierbei sind insbesondere Abhängigkeiten zu den Fachverfahren zu berücksichtigen. Auch müssen die Wirtschaftlichkeitsaspekte ganzheitlich betrachtet werden, da den Einsparmöglichkeiten bei den Lizenzkosten meist erhöhte Schulungs-, Umstellungs-, und gegebenenfalls auch Anpassungsaufwände gegenüberstehen. Bisherige Praxisbeispiele haben gezeigt, dass die Kosten für die Absicherung eines kontinuierlichen Supports bei Open-Source-Lösungen potenziell höher liegen als bei den derzeit verbreiteten Closed-Source-Lösungen. Hinsichtlich der Interoperabilität, insbesondere bei Office-Anwendungen, ist proprietäre Software aufgrund ihres erheblich höheren Verbreitungsgrads derzeit noch klar im Vorteil."],
+			[1,"Die SPD hat diese Frage bislang nicht beantwortet."]
 		],
 		[
 			[2,"Freie W-LAN-Angebote wollen wir weiter ausbauen. Förderung für freie und nicht-kommerzielle W-LAN-Angebote wie Freifunk auch für Kommunen öffnen"],
 			[2,"Wir werden den Aufbau von Freifunknetzen in Nordrhein-Westfalen weiterhin unterstützen. Das auf der W-LAN-Technologie basierende Netz ist, unabhängig von jeglicher Kontrolle und Zensur, zeitlich unbegrenzt verfügbar."],
-			[0,"These 13.04 des Wahlprogramms der AfD für NRW: \"Wir fordern die diskriminierungsfreie, öffentliche Ausschreibung aller kommunaler Ver- und Entsorgungsverträge.\""]
+			[0,"These 13.04 des Wahlprogramms der AfD für NRW: \"Wir fordern die diskriminierungsfreie, öffentliche Ausschreibung aller kommunaler Ver- und Entsorgungsverträge.\""],
+			[0,"Die Freien Demokraten NRW setzen sich für flächendeckende und leistungsfähige digitale Infrastrukturen ein. Dazu gehören auch offene WLAN-Netze. Viele Freifunk-Initiativen engagieren sich hier bereits. Aber auch jedes Café, jeder mittelständische Betrieb, jedes IT-Unternehmen und jede öffentliche Einrichtung, die WLAN-Zugänge für die Bürgerinnen und Bürger schaffen, leisten einen Beitrag. Deshalb setzt sich die FDP u.a. auch dafür ein, dass jede Behörde und jede Einrichtung des Landes offenes WLAN anbietet."],
+			[1,"Wir wollen uns für den Ausbau des mobilen Netzes sowie einer kostenlosen WLAN-Infrastruktur einsetzen. Wir erwarten durch die Änderung des Telemediengesetzes mit der Abschaffung der sog. Störerhaftung eine flächendeckende Verbreitung offener Netze."],
+			[1,"Die SPD hat diese Frage bislang nicht beantwortet."]
 		],
 		[
 			[2,"Wir setzen uns für ein verstärktes Angebot und die Nutzung von Open Data ein. Das Informationsfreiheitsgesetz NRW wollen wir zu einem Transparenzgesetz nach Hamburger Vorbild ausbauen. Staatliche Informationen müssen grundsätzlich in maschinenlesbaren Formaten im Internet auf einer geeigneten Plattform zur Verfügung stehen."],
 			[2,"Um Innovationen, Journalismus und bürgerschaftliches Engagement zu unterstützen, wollen wir die Bereitstellung von öffentlichen Informationen als OpenData nach den “Open Data Principles” im Transparenzgesetz festschreiben. Langfristig wollen wir damit unter anderem eine Entlastung der öffentlichen Verwaltung erreichen, da interne Informations- und Berichtsabfragen innerhalb der Verwaltung automatisiert werden können."],
-			[2,"Die AfD tritt grundsätzlich für eine Stärkung der direkten Demokratie ein. Dies ist nur möglich, wenn den Bürgern auch Informationen zu Verfügung stehen. Ausnahmen sollten aber möglich sein, wenn dies die Innere Sicherheit fordert."]
+			[2,"Die AfD tritt grundsätzlich für eine Stärkung der direkten Demokratie ein. Dies ist nur möglich, wenn den Bürgern auch Informationen zu Verfügung stehen. Ausnahmen sollten aber möglich sein, wenn dies die Innere Sicherheit fordert."],
+			[2,"Transparenz und Bürgernutzen der Verwaltung unter der Maßgabe, dass der Datenschutz ausreichend sichergestellt ist, haben für die Freien Demokraten NRW einen hohen Stellenwert."],
+			[0,"Dies sollte im Ermessen der jeweils die Daten pflegenden Stelle liegen. Das Informationsfreiheitsgesetz NRW bildet insoweit den gesetzlichen Rahmen."],
+			[1,"Die SPD hat diese Frage bislang nicht beantwortet."]
 		],
 		[
 			[2,"Wir setzen uns für eine Förderung von Open Access an Hochschulen in NRW ein. Das Land muss eine stringente Förderstrategie entwickeln und umsetzen."],
 			[2,"Aus öffentlicher Hand (teil-)finanzierte wissenschaftliche Informationen und Forschungsergebnisse sollen auch der Öffentlichkeit zugute kommen und damit für alle Bürger einfach und frei zugänglich sein."],
-			[2,"Die AfD tritt grundsätzlich für eine Stärkung der direkten Demokratie ein. Dies ist nur möglich, wenn den Bürgern auch Informationen zu Verfügung stehen. Ausnahmen sollten aber möglich sein, wenn dies die Innere Sicherheit fordert."]
+			[2,"Die AfD tritt grundsätzlich für eine Stärkung der direkten Demokratie ein. Dies ist nur möglich, wenn den Bürgern auch Informationen zu Verfügung stehen. Ausnahmen sollten aber möglich sein, wenn dies die Innere Sicherheit fordert."],
+			[2,"Wir wollen aus öffentlichen Drittmitteln geförderte Forschung an die Bedingung knüpfen, dass die Forschungsergebnisse obligatorisch als Open-Access-Publikation veröffentlicht werden. Die Landesregierung sollte dabei mit gutem Beispiel vorangehen und alle Forschungsvorhaben und Gutachten, die von ihr in Auftrag gegeben wurden, online für jedermann zugänglich machen."],
+			[0,"Es sollte den Erstellenden überlassen sein, über eine Freigabe zu entscheiden. Es gehört zur Wissenschaftsfreiheit, wie die Autoren ihre wissenschaftlichen Arbeiten veröffentlichen wollen. Die freie Nutzung sollte gefördert und unterstützt werden."],
+			[1,"Die SPD hat diese Frage bislang nicht beantwortet."]
 		],
 		[
 			[2],
 			[2,"Wir werden im Landtag, über den Einfluss durch den Bundesrat, eine Reform des Urheberrechts einleiten. Diese Reform soll in der Hinsicht in das bestehende Urheberrecht eingreifen, als dass es sich dem digitalen Wandel nicht mehr verschließt, Missverständnisse und Missstände ausräumt und das in Schieflage geratene Gleichgewicht zwischen Urhebern, Rechteverwertern und der Allgemeinheit zugunsten der Kulturschaffenden und Verbraucher wiederherstellt."],
-			[1]
+			[1],
+			[2,"Kultur braucht Freiheit. Das gilt auch für Museen."],
+			[0,"Die Museen sollten hier frei entscheiden können. Wir beziehen uns diesbezüglich auf den Antrag der CDU-Landtagsfraktion (Drucksache 16/10422) zum Thema: „Den Reichtum unserer Museen in Nordrhein-Westfalen durch Digitalisierung besser sichtbar machen – praxistaugliches Urheberecht zur Digitalisierung von Museumsbeständen einführen!"],
+			[1,"Die SPD hat diese Frage bislang nicht beantwortet."]
 		]
 	]
 };
